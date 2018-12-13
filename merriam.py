f80baa8dd0d3da9d342718342dd687883797338f
fluencer <fluencer@gmail.com>
2018-12-12 19:40:18 -0500

    Printing word usage added

--- merriam.py
+++ merriam.py
@@ -4,38 +4,25 @@
 # Description:
 # Author: Vidhyalakshmi Sundara Raman
 # Created: Mon Nov  5 01:06:14 2018 (-0500)
-# Version:
-# Packages-Required: ()
-# Last-Updated:
-#           By:
-#     Update #: 0
-# Keywords:
-# Compatibility:
-#
-#
 
 # TODO:
 # [ ] Lookup for queries with more than a word
-# [ ] Get usage examples for query
+# [X] Get usage examples for query
 # [ ] Key in separate folder
 #
 
-# Change Log:
-#
-#
-#
+# Test cases: hello, please, love
 
 # Code:
 
 import json
-import sys
 import argparse
 import urllib.request
+import re
 
-keyfile = open("keys.txt", 'r')
+keyfile = open("/Users/vsr/myprojects/pyDumb/dictionary-app/keys.txt", 'r')
 keys = keyfile.readline()
 parser = argparse.ArgumentParser()
-# query = input("Lookup word:")
 parser.add_argument("word", type=str)
 args = parser.parse_args()
 query = args.word
@@ -45,6 +32,8 @@ jsStruct = json.load(response)
 
 for meaning in jsStruct:
     definitions = meaning['shortdef']
+    if meaning['meta']['id'] != query:
+        print("\n"+meaning['meta']['id'])
     try:
         print('('+meaning['fl']+')')
         for i, eachDef in enumerate(definitions, 1):
@@ -52,5 +41,36 @@ for meaning in jsStruct:
     except KeyError:
         pass
 
+print("\nUsage")
+
+for usage in jsStruct:
+    try:
+        longdefs = usage['def']
+        for i, eachDef in enumerate(longdefs, 1):
+            if 'sseq' in eachDef:
+                # test[*][0][1]['dt'] - gives list of illustrations
+                for ill in eachDef['sseq']:
+                    if 'dt' in ill[0][1]:
+                        for illus in ill[0][1]['dt']:
+                            if illus[0] == 'vis':
+                                # enumerate
+                                for ii in range(1, len(illus)):
+                                    L = len(illus[ii])
+                                    for jj in range(L):
+                                        if 't' in illus[ii][jj]:
+                                            # remove everything within braces before printing
+                                            print("* "+illus[ii][jj]['t'])
+    except KeyError:
+        pass
+            
+            
+        
+# function to strip formatted strings like {a_link|pleasure} to pleasure
+def stripString(query, sentence):
+    # if the query is surrounded by brackets
+    pass
+    
+    
+# # the first word can be anything
 #
 # merriam.py ends here
